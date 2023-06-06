7f53f4204eb254c3dce2cfcf2f193d91ee2e99f3
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-06 16:07:44 -0700

    starts base auth in app

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import { getUser } from "../../utilities/users-service";
+import NavBar from "../../components/NavBar/NavBar";
+import AuthPage from "../AuthPage/AuthPage";
 
 function App() {
+  const [user, setUser] = useState(getUser());
+
   return (
     <div className="App">
+      {user ? (
+        <>
+          <NavBar user={user} setUser={setUser} />
+          <Routes>{/* insert routes for users */}</Routes>
+        </>
+      ) : (
+        <AuthPage path="/" setUser={setUser} />
+      )}
       <h1>App</h1>
       <p>Spanish Flashcard App</p>
     </div>
