f561c7cc3885e7fa102f39d68aae1fa37303c84f
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-06 16:05:24 -0700

    fixes payload typo

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -2,10 +2,9 @@ import { getToken } from "./users-service";
 
 export default async function sendRequest(url, method = "GET", payload = null) {
   const options = { method };
-  if (token) {
+  if (payload) {
     options.headers ||= {};
-
-    options.header.Authorization = `Bearer ${token}`;
+    options.header.Authorization = `Bearer ${payload}`;
   }
 
   const res = await fetch(url, options);
