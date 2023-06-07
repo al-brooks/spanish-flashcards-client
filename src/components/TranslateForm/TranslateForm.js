35145a8ea751640ae1e1df0eb444caf18c53fb0f
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-07 13:57:04 -0700

    sets up translation form

--- src/components/TranslateForm/TranslateForm.js
+++ src/components/TranslateForm/TranslateForm.js
@@ -1,9 +1,45 @@
 import { useState } from "react";
+import * as translateServices from "../../utilities/translate-service";
 
-export default function TranslateForm() {
+export default function TranslateForm({ setTranslation }) {
+  const [search, setSearch] = useState({
+    searchTerm: ""
+  });
+
+  const [error, setError] = useState("");
+
+  const handleSubmit = async evt => {
+    evt.preventDefault();
+    try {
+      const result = await translateServices.getTranslation(search);
+      setTranslation(result);
+    } catch {
+      setError("Oops, we weren't able to find this...");
+    }
+  };
+
+  const handleChange = evt => {
+    setSearch({
+      ...search,
+      [evt.target.name]: evt.target.value
+    });
+    setError("");
+  };
+
   return (
     <section>
       <h2>Translate Words in English and Spanish</h2>
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <input
+          type="text"
+          name="searchTerm"
+          value={search.searchTerm}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Search</button>
+      </form>
+      <p className="error-msg">&nbsp;{error}</p>
     </section>
   );
 }
