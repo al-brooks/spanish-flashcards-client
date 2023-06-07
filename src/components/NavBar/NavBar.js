aaab3d121df81abb7a7a31fbc32a35ebcd955f78
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-07 13:28:46 -0700

    builds out nav with logout

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,3 +1,18 @@
-export default function NavBar() {
-  return <h1>NavBar</h1>;
+import { Link } from "react-router-dom";
+import * as usersService from "../../utilities/users-service";
+
+export default function NavBar({ user, setUser }) {
+  const handleLogOut = () => {
+    usersService.logOut();
+    setUser(null);
+  };
+
+  return (
+    <nav>
+      <Link to={"/"}>Home </Link>
+      <Link to="" onClick={handleLogOut}>
+        Log Out
+      </Link>
+    </nav>
+  );
 }
