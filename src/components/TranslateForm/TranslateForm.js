b93914c221b1d6117d468b711f799f6749085492
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-07 10:53:28 -0700

    sets up init translate form

---
+++ src/components/TranslateForm/TranslateForm.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export default function TranslateForm() {
+  return (
+    <section>
+      <h2>Translate Words in English and Spanish</h2>
+    </section>
+  );
+}
