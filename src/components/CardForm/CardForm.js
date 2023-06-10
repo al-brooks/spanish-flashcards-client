095046210ddabb705b20b6011fab9432c813011f
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-10 10:59:15 -0700

    renames and adds create card form

---
+++ src/components/CardForm/CardForm.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as flashcardsService from "../../utilities/flashcards-service";
+
+export default function CardForm() {
+  const [card, setCard] = useState({
+    content: "",
+    translation: "",
+    difficulty: ""
+  });
+
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = evt => {
+    setCard({
+      ...card,
+      [evt.target.name]: evt.target.value
+    });
+    setError("");
+  };
+
+  const handleSubmit = async evt => {
+    evt.preventDefault();
+    try {
+      const response = await flashcardsService.createCard(card);
+      if (response) navigate({ pathname: `/flashcards/decks/${response._id}` });
+    } catch {
+      setError("Card couldn't be created - Try again");
+    }
+  };
+
+  return (
+    <>
+      <h3>Deck Form</h3>
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <label>Front</label>
+        <input
+          type="text"
+          name="content"
+          value={card.content}
+          onChange={handleChange}
+          required
+        />
+        <label>Back</label>
+        <input
+          type="text"
+          name="translation"
+          value={card.translation}
+          onChange={handleChange}
+          required
+        />
+        <label>Difficulty</label>
+        <select name="difficulty">
+          <option value="New">New</option>
+          <option value="Tough">Tough</option>
+          <option value="Okay">Okay</option>
+          <option value="Easy">Easy</option>
+          <option value="Fluent">Fluent</option>
+        </select>
+        <button type="submit">Create</button>
+      </form>
+      <p className="error-msg">&nbsp;{error}</p>
+    </>
+  );
+}
--- src/components/CreateFlashcardForm/CreateFlashcardForm.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-export default function CreateFlashcardForm({ setFlashCard }) {
-  return (
-    <div>
-      <h3>Create Flashcard Form</h3>
-    </div>
-  );
-}
