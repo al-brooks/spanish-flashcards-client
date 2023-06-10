3e1d75b63fd47923aa7a0b7b064cdb67259f9dd9
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-10 11:15:40 -0700

    starts create page build

--- src/pages/CreatePage/CreatePage.js
+++ src/pages/CreatePage/CreatePage.js
@@ -1,11 +1,23 @@
+import { useParams } from "react-router-dom";
 import DeckForm from "../../components/DeckForm/DeckForm";
 import CardForm from "../../components/CardForm/CardForm";
 
 export default function CreateDeckPage() {
+  const deckId = useParams()?.d_id;
+
   return (
     <main>
-      <h1>Create Deck Page</h1>
-      <DeckForm />
+      {!deckId ? (
+        <>
+          <h1>Create Deck Page</h1>
+          <DeckForm />
+        </>
+      ) : (
+        <>
+          <h1>Create Deck Page</h1>
+          <CardForm />
+        </>
+      )}
     </main>
   );
 }
