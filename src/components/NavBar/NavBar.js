db1d83ac09d09df3b182d381f7c8905917539690
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-11 12:13:44 -0700

    more styles

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,8 +8,10 @@ export default function NavBar({ user, setUser }) {
   };
 
   return (
-    <nav className="flex flex-even">
-      <Link to={"/"}>Home</Link>
+    <nav className="flex flex-even flex-align-end">
+      <Link to={"/"}>
+        Spanish<span className="color-span">Flash</span>
+      </Link>
       <Link to={"/translate"}>Translate</Link>
       <Link to={"/flashcards"}>Flashcards</Link>
       <Link to="" onClick={handleLogOut}>
