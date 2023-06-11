07eb92c4050e4a5a05fd050075c1502309e73b88
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-10 18:42:07 -0700

    builds out card detail page

---
+++ src/pages/CardDetailPage/CardDetailPage.js
@@ -0,0 +1,59 @@
+import { useState, useEffect, useCallback } from "react";
+import { Link, useParams } from "react-router-dom";
+import * as flashcardsService from "../../utilities/flashcards-service";
+
+export default function CardDetailPage() {
+  const [toggleCard, setToggleCard] = useState(false);
+  const [card, setCard] = useState(null);
+  const [error, setError] = useState("");
+  const { d_id, c_id } = useParams();
+
+  const handleToggle = () => {
+    setToggleCard(!toggleCard);
+  };
+
+  const fetchCard = useCallback(async () => {
+    try {
+      const response = await flashcardsService.showCard(d_id, c_id);
+      if (response) {
+        setCard(response);
+        setError("");
+      }
+    } catch {
+      setError("Oh no! Something went wrong...");
+    }
+  }, [c_id]);
+
+  useEffect(() => {
+    fetchCard();
+  }, [fetchCard]);
+
+  const loading = () => {
+    if (error) {
+      return <h2>{error}</h2>;
+    }
+    return <h2>Loading Info...</h2>;
+  };
+
+  const loaded = () => {
+    return (
+      <main>
+        <h1>Card Detail Page</h1>
+        <Link to={`/flashcards/decks/${d_id}`}>Back</Link>
+        {!toggleCard ? (
+          <article>
+            <p>Front of Card</p>
+            <button onClick={handleToggle}>Back</button>
+          </article>
+        ) : (
+          <article>
+            <p>Back of Card</p>
+            <button onClick={handleToggle}>Front</button>
+          </article>
+        )}
+        <p className="error-msg">&nbsp;{error}</p>
+      </main>
+    );
+  };
+  return <>{card ? loaded() : loading()}</>;
+}
