d8abfcf399e8586eea4b4556905a6ddc56d7df22
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-11 13:48:41 -0700

    updates deck card functionality

--- src/pages/DeckListPage/DeckListPage.js
+++ src/pages/DeckListPage/DeckListPage.js
@@ -10,6 +10,7 @@ export default function DeckListPage() {
   const fetchDecks = useCallback(async () => {
     try {
       const response = await flashcardsService.getAllDecks();
+      console.log(response);
       setDecks(response);
       setError("");
     } catch {
@@ -26,25 +27,25 @@ export default function DeckListPage() {
       <section className="deck-list-header">
         <div>
           <h2>Flashcard Decks</h2>
-          {decks.length === 1 ? (
-            <p>
-              <span className="color-word">1</span> Deck
-            </p>
-          ) : (
-            <p>
-              <span className="color-word">{decks.length}</span> Decks
-            </p>
-          )}
+          <p>
+            <span className="color-word">{decks.length}</span>
+            {decks.length === 1 ? " Deck" : " Decks"}
+          </p>
         </div>
         <Link to={"/flashcards/decks/create"}>&#43; Create Deck</Link>
       </section>
       <section>
+        <h3>My Decks:</h3>
         {decks.length > 0 ? (
           decks.map(deck => {
             return (
               <Link key={deck._id} to={`/flashcards/decks/${deck._id}`}>
-                <article>
-                  <h4>{deck.name}</h4>
+                <article className="deck-card">
+                  <h3>{deck.name}</h3>
+                  <p>
+                    <span className="color-word">{deck.cards.length}</span>
+                    {deck.cards.length === 1 ? " Card" : " Cards"}
+                  </p>
                 </article>
               </Link>
             );
