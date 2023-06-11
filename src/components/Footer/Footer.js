5e074135369d6bf86fdadc2ab517fa7032b817e9
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-11 10:14:00 -0700

    more landing styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,24 +5,20 @@ export default function Footer() {
   return (
     <footer>
       <p>
-        Copyright &copy; All Rights Reserved {new Date().getFullYear()}{" "}
-        Flashcard App
+        Copyright &copy; All Rights Reserved {new Date().getFullYear()} Spanish
+        Flashcards
       </p>
       <p>
         Translation API Provided By: Merriam-Webster's Spanish-English
         Dictionary with Audio API
         <br />
         <em>Merriam-Webster Inc.</em>
-        <Link
-          to={"https://www.dictionaryapi.com/products/api-spanish-dictionary"}
-        >
-          <img
-            style={{ width: "50px", height: "50px" }}
-            src={mwLogo}
-            alt="Merriam-Webster Inc. Logo"
-          />
-        </Link>
       </p>
+      <Link
+        to={"https://www.dictionaryapi.com/products/api-spanish-dictionary"}
+      >
+        <img className="mw-logo" src={mwLogo} alt="Merriam-Webster Inc. Logo" />
+      </Link>
     </footer>
   );
 }
