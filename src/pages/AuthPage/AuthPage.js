e1510957b262a61d40a668db0b07a5d69b5a4647
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-11 19:08:50 -0700

    styles landing

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,3 +1,4 @@
+import "./AuthPage.css";
 import { useState } from "react";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import LoginForm from "../../components/LoginForm/LoginForm";
@@ -10,24 +11,39 @@ export default function AuthPage({ setUser }) {
   };
 
   return (
-    <>
-      {formToggle ? (
-        <section className="AuthPage">
-          <h3>Not a Member?</h3>
-          <button className="btn btn-color-cancel" onClick={handleToggle}>
-            Sign Up
-          </button>
-          <LoginForm setUser={setUser} />
-        </section>
-      ) : (
-        <section className="AuthPage">
-          <h3>Already Have a Login?</h3>
-          <button className="btn btn-color-cancel" onClick={handleToggle}>
-            Log In
-          </button>
-          <SignUpForm setUser={setUser} />
-        </section>
-      )}
-    </>
+    <section className="AuthPage">
+      <section className="flex flex-col">
+        {formToggle ? (
+          <>
+            <h3>Not a Member?</h3>
+            <button className="btn btn-color-cancel" onClick={handleToggle}>
+              Sign Up
+            </button>
+            <LoginForm setUser={setUser} />
+          </>
+        ) : (
+          <>
+            <h3>Already Have a Login?</h3>
+            <button className="btn btn-color-cancel" onClick={handleToggle}>
+              Log In
+            </button>
+            <SignUpForm setUser={setUser} />
+          </>
+        )}
+      </section>
+      <section className="welcome">
+        <p>
+          <span className="color-blue">Spanish</span>
+          <span className="color-word">Flash</span> allows you to create
+          flashcards to help you with your Spanish! While it's tailored
+          specifically to English - Spanish translationas, you can also create
+          custom cards for all your studying needs.
+        </p>
+        <p>
+          Test out our translation tool below! Just search the word you'd like
+          translated.
+        </p>
+      </section>
+    </section>
   );
 }
