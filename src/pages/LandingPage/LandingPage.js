8b325adefbd81ee8a5588159377ad71a0b370efa
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-11 18:59:23 -0700

    updates welcome text

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -5,9 +5,11 @@ export default function LandingPage() {
     <section className="LandingPage">
       <h1>¡Bienvenido!</h1>
       <p>
-        This site allows you to create flashcards to help you with your Spanish!
-        While it's tailored specifically to English - Spanish translationas, you
-        can also create custom cards for all your studying needs.
+        <span className="color-blue">Spanish</span>
+        <span className="color-word">Flash</span> allows you to create
+        flashcards to help you with your Spanish! While it's tailored
+        specifically to English - Spanish translationas, you can also create
+        custom cards for all your studying needs.
       </p>
     </section>
   );
