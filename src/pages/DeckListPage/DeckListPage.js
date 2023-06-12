37ba6adc0a1f65bc8e3503c3c2d8f5ab73f69392
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-11 19:40:25 -0700

    awaits load for deck list

--- src/pages/DeckListPage/DeckListPage.js
+++ src/pages/DeckListPage/DeckListPage.js
@@ -22,49 +22,60 @@ export default function DeckListPage() {
     fetchDecks();
   }, [fetchDecks]);
 
-  return (
-    <>
-      <section className="deck-list-header">
-        <div>
-          <h2>Flashcard Decks</h2>
-          <p>
-            <span className="color-word">{decks.length}</span>
-            {decks.length === 1 ? " Deck" : " Decks"}
-          </p>
-        </div>
-        <Link to={"/flashcards/decks/create"}>&#43; Create Deck</Link>
-      </section>
-      <h3>My Decks:</h3>
-      <section className="decks-wrapper">
-        {decks.length > 0 ? (
-          decks.map((deck, idx) => {
-            return idx % 2 === 0 ? (
-              <article key={deck._id} className="deck-card deck-card-even">
-                <Link to={`/flashcards/decks/${deck._id}`}>
-                  <h3>{deck.name}</h3>
-                  <p>
-                    <span className="color-word">{deck.cards.length}</span>
-                    {deck.cards.length === 1 ? " Card" : " Cards"}
-                  </p>
-                </Link>
-              </article>
-            ) : (
-              <article key={deck._id} className="deck-card deck-card-odd">
-                <Link to={`/flashcards/decks/${deck._id}`}>
-                  <h3>{deck.name}</h3>
-                  <p>
-                    <span className="color-word">{deck.cards.length}</span>
-                    {deck.cards.length === 1 ? " Card" : " Cards"}
-                  </p>
-                </Link>
-              </article>
-            );
-          })
-        ) : (
-          <p>No decks so far...</p>
-        )}
-      </section>
-      {error ? <p className="error-msg">&nbsp;{error}</p> : <></>}
-    </>
-  );
+  const loading = () => {
+    if (error) {
+      return <h2>{error}</h2>;
+    }
+    return <h2>Loading Info...</h2>;
+  };
+
+  const loaded = () => {
+    return (
+      <>
+        <section className="deck-list-header">
+          <div>
+            <h2>Flashcard Decks</h2>
+            <p>
+              <span className="color-word">{decks.length}</span>
+              {decks.length === 1 ? " Deck" : " Decks"}
+            </p>
+          </div>
+          <Link to={"/flashcards/decks/create"}>&#43; Create Deck</Link>
+        </section>
+        <h3>My Decks:</h3>
+        <section className="decks-wrapper">
+          {decks.length > 0 ? (
+            decks.map((deck, idx) => {
+              return idx % 2 === 0 ? (
+                <article key={deck._id} className="deck-card deck-card-even">
+                  <Link to={`/flashcards/decks/${deck._id}`}>
+                    <h3>{deck.name}</h3>
+                    <p>
+                      <span className="color-word">{deck.cards.length}</span>
+                      {deck.cards.length === 1 ? " Card" : " Cards"}
+                    </p>
+                  </Link>
+                </article>
+              ) : (
+                <article key={deck._id} className="deck-card deck-card-odd">
+                  <Link to={`/flashcards/decks/${deck._id}`}>
+                    <h3>{deck.name}</h3>
+                    <p>
+                      <span className="color-word">{deck.cards.length}</span>
+                      {deck.cards.length === 1 ? " Card" : " Cards"}
+                    </p>
+                  </Link>
+                </article>
+              );
+            })
+          ) : (
+            <p>No decks so far...</p>
+          )}
+        </section>
+        {error ? <p className="error-msg">&nbsp;{error}</p> : <></>}
+      </>
+    );
+  };
+
+  return <>{decks ? loaded() : loading()}</>;
 }
