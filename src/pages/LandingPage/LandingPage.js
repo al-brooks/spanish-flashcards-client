28790b50e14b32828f454b776c4c24837ad49746
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-06-11 18:34:24 -0700

    finishes styles for landing pg

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,12 +1,14 @@
+import "./LandingPage.css";
+
 export default function LandingPage() {
   return (
-    <>
+    <section className="LandingPage">
       <h1>¡Bienvenido!</h1>
       <p>
         This site allows you to create flashcards to help you with your Spanish!
         While it's tailored specifically to English - Spanish translationas, you
         can also create custom cards for all your studying needs.
       </p>
-    </>
+    </section>
   );
 }
